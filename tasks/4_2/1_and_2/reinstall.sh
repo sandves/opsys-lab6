060c1790f9e6ebdfc22b5a1a416b1ed44f1c4c6c
Stian Sandve <stian@sandve.org>
2013-11-08 17:11:21 +0100

    Update reinstall.sh

--- tasks/4_2/1_and_2/reinstall.sh
+++ tasks/4_2/1_and_2/reinstall.sh
@@ -5,5 +5,5 @@ rm /dev/simp_malloc
 make
 insmod simp_malloc.ko
 mknod /dev/simp_malloc c 245 0
-sudo chgrp wheel /dev/simp_malloc
-sudo chmod 664 /dev/simp_malloc
+chgrp wheel /dev/simp_malloc
+chmod 664 /dev/simp_malloc
